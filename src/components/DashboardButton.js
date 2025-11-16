24d37702428a89ba390d2ddcabd5caedb755d1bc
ifti56 <ifti1909056@stud.kuet.ac.bd>
2025-11-16 12:49:28 +0600

    Update DashboardButton.js

--- src/components/DashboardButton.js
+++ src/components/DashboardButton.js
@@ -1,7 +1,12 @@
 export function DashboardButton() {
   return (
+    <>
       <button className="dashboard-btn">
           Go to Dashboard →
+          this line shouldn't be detected as changed
       </button>
+
+      <h1>this should include in PR </h1>
+    </>
   );
 }
