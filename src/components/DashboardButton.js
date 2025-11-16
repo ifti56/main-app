eee5bde1c70eb98851c876590bf1e23f2e001f89
ifti56 <ifti1909056@stud.kuet.ac.bd>
2025-11-16 15:58:28 +0600

    Update DashboardButton.js

--- src/components/DashboardButton.js
+++ src/components/DashboardButton.js
@@ -3,7 +3,7 @@ export function DashboardButton() {
     <>
       <button className="dashboard-btn">
           Go to Dashboard →
-          this line shouldn't be detected as chandfsdfsdgedddd
+          this line shouldn't be detected as changed
       </button>
 
       <h1>this should include in PR </h1>
